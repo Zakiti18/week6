b629c0a845c96e11a3e89b1fe18db88d3faed4b6
Phillip Ball <pball@mail.greenriver.edu>
2021-05-06 13:52:03 -0400

    More templating practice

--- index.php
+++ index.php
@@ -15,10 +15,26 @@ $f3 = Base::instance();
 
 // define routes
 // default route
-$f3->route('GET /', function (){
+$f3->route('GET /', function ($f3){
+    // save variables to the F3 "hive"
+    $f3->set('username', 'Jshmo');
+    $f3->set('password', sha1('Password01'));
+    $f3->set('title', 'Working with Templates');
+
+    $f3->set("color", 'green');
+    $f3->set("radius", '10');
+    $f3->set("fruits", array('apple', 'orange', 'banana'));
+
+    $f3->set('desserts', array('chocolate' => 'Chocolate Mousse',
+        'vanilla' => 'Vanilla Custard',
+        'strawberry' => 'Strawberry Shortcake'));
+
+    $f3->set('preferredCustomer', 'true');
+    $f3->set('lastLogin', strtotime('-1 week'));
+
     // display the home page
     $view = new Template();
-    echo $view->render('views/home.html');
+    echo $view->render('views/info.html');
 });
 
 // run Fat-Free
